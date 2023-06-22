de15661f75d771973364fe1a61f2fb2b7095cb1a
Sérgio Azevedo <sergio.azevedo@constructionconcept.eu>
2023-06-22 18:02:21 +0100

    initial commit

---
+++ db.py
@@ -0,0 +1,44 @@
+from deta import Deta
+from pprint import pprint
+import streamlit as st
+
+DETA_KEY = st.secrets.DETA_KEY
+deta = Deta(DETA_KEY)
+
+sl = deta.Base("shoppingList")
+
+#---SHOPPING LIST FUNCTIONS---#
+def enter_shopping_list_items(key, weeknumber, shopping_list, bought):
+    """This function will add all items in the shopping_list dictionary and the week title to the database with the week number as a key. Will return None if successful. """
+    return sl.put({"key":str(key), "weeknumber":weeknumber, "shopping_list":shopping_list, "bought":bought})
+
+
+def get_shopping_list(weeknumber):
+    """Returns the entire shopping list item for a certain week based on the period. This period is the week title used in the function above."""
+    return sl.get({"weeknumber":int(weeknumber)})
+
+def getAllItems():
+    return sl.fetch().items
+
+def updateItem(grocery, key):
+
+    if grocery["bought"] == True:
+        grocery["bought"] = False
+    else:
+        grocery["bought"] = True
+    
+    shopping_list_update_line = {f"bought": grocery["bought"]}
+
+    sl.update(shopping_list_update_line, key)
+           
+
+def remove_item_shopping_list(weeknumber, item_cat, item_to_remove):
+    """Function  to remove a single item from the list. It takes a weeknumber as the principal key to get the list for the correct week. The item_cat selects the category and the item_to_remove is the actual item to get rid off. The same update function as above is then used to apply the changes."""
+    shopping_list_to_change = get_shopping_list(weeknumber)["shopping_list"][item_cat]['items']
+    shopping_list_to_change.remove(item_to_remove)
+    print(shopping_list_to_change)
+   
+    shopping_list_update_line = {
+        f"shopping_list.{item_cat}.items": shopping_list_to_change
+        }
+    sl.update(shopping_list_update_line, weeknumber)
---
+++ shoppingList.py
@@ -0,0 +1,90 @@
+#---PIP PACKAGES---#
+import streamlit as st
+from streamlit_option_menu import option_menu
+from isoweek import Week
+import random
+
+#---BUILT-IN PYTHON MODULES
+from datetime import datetime, date
+import calendar
+import uuid
+
+#---IMPORT THE DATABASE PYTHON FILE db.py---#
+import db as db
+
+#---STREAMLIT SETTINGS---#
+pageTitle = "Weekly Shopping List App"
+pageIcon = ":pouch:"
+layout = "centered"
+
+#---STREAMLIT PAGE CONFIG---#
+st.set_page_config(page_title=pageTitle, page_icon=pageIcon, layout=layout)
+st.title(f"{pageTitle} {pageIcon}")
+
+#---STREAMLIT CONFIG HIDE---#
+hide_st_style = """<style>
+                #MainMenu {visibility : hidden;}
+                footer {visibility : hidden;}
+                header {visibility : hidden;}
+                </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+#---PERIOD VALUES---#
+year = datetime.today().year
+month = datetime.today().month
+day = datetime.today().day + 4
+months = list(calendar.month_name[1:])
+week_number = date(year, month, day).isocalendar()[1]
+week = Week(year, week_number)
+week_plus1 = Week(year, week_number + 1)
+
+
+#---NAV BAR---#
+nav_menu = option_menu(
+    menu_title = None,
+    options = ["Current Week", "History"],
+    icons = ["list-task", "cup-straw" ],
+    orientation = "horizontal"
+)
+
+if nav_menu == "Current Week":
+    st.header(f"Monday {week.monday()} to Sunday {week.sunday()}")
+
+
+    with st.form("entry_form", clear_on_submit=True):
+                    
+        shoppingList = st.text_input("Groceries","")
+        
+        submitted = st.form_submit_button("Save item", type = "primary")  
+
+    if submitted:               
+                key = uuid.uuid4()
+
+                db.enter_shopping_list_items(key, week_number, shoppingList, False)
+
+
+    with st.expander(label="List of Groceries", expanded=True):
+        
+        current_shopping_list = db.getAllItems()
+
+        if current_shopping_list:
+            for grocery in current_shopping_list:
+                if grocery["weeknumber"] == week_number:
+                    st.checkbox(label = grocery["shopping_list"], value = grocery["bought"], 
+                                on_change=db.updateItem, args=(grocery, str(grocery["key"])))    
+        else:
+            st.caption(f"You have not created a shopping list yet for week from Thursday {week.thursday()} to Wednesday {week_plus1.wednesday()}")
+
+
+#---HISTORY TAB---#
+
+if nav_menu == "History":
+
+    current_shopping_list = db.getAllItems()
+
+    for wk in range(52):
+        with st.expander(label=f"Semana {wk}", expanded=False):
+            for grocery in current_shopping_list:
+                if grocery["weeknumber"] == wk:
+                    st.checkbox(label = grocery["shopping_list"], value = grocery["bought"], disabled=True)    
